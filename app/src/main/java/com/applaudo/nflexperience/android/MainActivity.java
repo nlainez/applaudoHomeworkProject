4f699ab9bc89a491d39b87e1e161339b40fcb785
shinmen14 <shinmen14@gmail.com>
2015-05-28 20:33:27 -0600

    Implementing compatibility features
    -MainActivity now inherits from AppCompatActivity
    -Theme changed to Theme.AppCompat
    -Added colors.xml file for NFL approved colors.

--- app/src/main/java/com/applaudo/nflexperience/android/MainActivity.java
+++ app/src/main/java/com/applaudo/nflexperience/android/MainActivity.java
@@ -1,12 +1,12 @@
 package com.applaudo.nflexperience.android;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends FragmentActivity
+public class MainActivity extends AppCompatActivity
         implements VenueListFragment.OnVenueSelectedListener{
 
     @Override
